0646218238dc1d3ed8be3d0601ab523ea79b8916
haysahakyan <sahakyanhayarpi0@gmail.com>
2022-06-17 23:19:56 -0700

    main functions done

--- js/hangman.js
+++ js/hangman.js
@@ -21,14 +21,11 @@ let mistakes = 0;
 let guessed = [];
 let wordStatus = null;
 
-
-// eny functions
 function randomWord() {
     answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
-
 }
 
-function generalButtons() {
+function generateButtons() {
     let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
         `
       <button
@@ -46,23 +43,63 @@ function handleGuess(chosenLetter){
     guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
     document.getElementById(chosenLetter).setAttribute('disabled', true);
 
-//console.log(answer)
+    console.log(answer)
 
     if(answer.indexOf(chosenLetter) >=0 ){
-        guessedWord()
+        guessedWord();
+        chekIfgameWon()
+        //correct image
+    }else if(answer.indexOf(chosenLetter) === -1) {
+        mistakes++;
+        updateMistakes();
+        chekIfgameLost();
+        updateHangmanPicture()
+    }
+}
+function updateHangmanPicture(){
+    // document.getElementById('hangmanPic').src='./images/giphy.gif'
+    //images
+}
+function chekIfgameWon(){
+    if(wordStatus === answer){
+document.getElementById('keyboard').innerHTML = 'You Won!!';
+//put image won
     }
 }
 
+function chekIfgameLost(){
+    if(mistakes === maxWrong){
+        document.getElementById(wordSpotlight).innerHTML = 'The Answer was: ' + answer;
+document.getElementById('keyboard').innerHTML = 'You Lost!!';
+//put image lost
+    }
+}
 function guessedWord(){
     wordStatus = answer.split('').map(letter=> (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
 
     document.getElementById('wordSpotlight').innerHTML = wordStatus
 }
 
+function updateMistakes(){
+    document.getElementById('mistakes').innerHTML = mistakes
+}
+
+function reset(){
+    mistakes=0;
+    guessed=[];
+
+    document.getElementById('hangmanPic').src = './images/giphy.gif';
+    randomWord();
+    guessedWord();
+    updateMistakes();
+    generateButtons()
+    
+
+}
 
 document.getElementById('maxWrong').innerHTML = maxWrong
 
 randomWord();
-generalButtons();
+generateButtons();
 guessedWord();
 handleGuess()
